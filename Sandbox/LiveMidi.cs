f7c7ae2c3444c89084cb2e51f803b0d9f9726ffb
Bear Grills <carletonmaestros@gmail.com>
2013-11-06 22:06:40 -0600

    FUCK YEAH MOTHERFUCKING INTEGRATION!!!

--- Sandbox/LiveMidi.cs
+++ Sandbox/LiveMidi.cs
@@ -20,7 +20,7 @@ using System.ComponentModel; //AsyncCompletedEventArgs
 
 
 
-namespace Orchestra.LiveMidi
+namespace Orchestra
 {
     class LiveMidi
     {
@@ -52,31 +52,20 @@ namespace Orchestra.LiveMidi
 
         private void VolumeChanged(float volume)
         {
-            outDevice.Send(new ChannelMessage(ChannelCommand.Controller, 11, (int)(volume*127)));
+            sequencer1.clock.Tempo = (60000000 / Convert.ToInt32(volume*500));
+            outDevice.Send(new ChannelMessage(ChannelCommand.Controller, 11, (int)(volume * 127)));
         }
+
         private void Initialize()
         {
             this.sequence1 = new Sanford.Multimedia.Midi.Sequence();
             this.sequencer1 = new Sanford.Multimedia.Midi.Sequencer();
-            sequencer1.Stop();
-            sequence1.LoadAsync("C:\\Users\\admin\\Desktop\\VirtualOrchestra\\Sample MIDIs\\g.mid");
+            sequence1.LoadAsync(@"C:\Users\admin\Desktop\VirtualOrchestra\Sample MIDIs\g.mid");
 
             //sequencer1.Stop() followed by sequencer1.Continue could be used to handle changing tempo
             //also, perhaps sequencer1.position could be used (ticks)
             //sequence1.GetLength()
-        }
-        private void HandleLoadCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            sequencer1.Start();
-        }
 
-        private void HandleChannelMessagePlayed(object sender, ChannelMessageEventArgs e)
-        {
-            outDevice.Send(e.Message);
-        }
-
-        protected void OnLoad(EventArgs e)
-        {
             if (OutputDevice.DeviceCount == 0)
             {
                 Console.WriteLine("No MIDI output devices available.");
@@ -86,7 +75,6 @@ namespace Orchestra.LiveMidi
                 try
                 {
                     outDevice = new OutputDevice(outDeviceID);
-                    //sequence1.LoadProgressChanged += HandleLoadProgressChanged;
                     sequence1.LoadCompleted += HandleLoadCompleted;
                 }
                 catch (Exception ex)
@@ -95,7 +83,26 @@ namespace Orchestra.LiveMidi
                 }
             }
 
-            //base.OnLoad(e);
+
+            //this.sequencer1.PlayingCompleted += new System.EventHandler(this.HandlePlayingCompleted);
+            sequencer1.ChannelMessagePlayed += HandleChannelMessagePlayed;
+            //this.sequencer1.SysExMessagePlayed += new System.EventHandler<Sanford.Multimedia.Midi.SysExMessageEventArgs>(this.HandleSysExMessagePlayed);
+            //this.sequencer1.Chased += new System.EventHandler<Sanford.Multimedia.Midi.ChasedEventArgs>(this.HandleChased);
+            //this.sequencer1.Stopped += new System.EventHandler<Sanford.Multimedia.Midi.StoppedEventArgs>(this.HandleStopped);
+        }
+
+        private void HandleLoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            var x = sequence1.GetLength();
+            sequencer1.Sequence = sequence1;
+            sequencer1.Start();
+        }
+
+
+
+        private void HandleChannelMessagePlayed(object sender, ChannelMessageEventArgs e)
+        {
+            outDevice.Send(e.Message);
         }
     }
 }
