91f2e5563da3f52139ba755685340f1b2443b354
Bear Grills <carletonmaestros@gmail.com>
2014-02-04 10:30:20 -0600

    Meeeeeeeeeerrrrrrrrrrgggggggeeeeeeeeeddddddddd

--- MIDI/MIDI.cs
+++ MIDI/MIDI.cs
@@ -65,7 +65,7 @@ namespace Orchestra
 
             // Initialize MIDI
             sequencer.Sequence = sequence;
-            LoadSong(@"C:\Users\admin\Desktop\VirtualOrchestra\Sample MIDIs\r.mid");
+            LoadSong(@"C:\Users\admin\Desktop\VirtualOrchestra\Sample MIDIs\s.mid");
 
             // Initialize timer
             timer.Elapsed += new ElapsedEventHandler(TimePassed);
