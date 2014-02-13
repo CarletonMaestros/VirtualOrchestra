125e58976db0dfe3446def707a83df90f60133c1
Bear Grills <carletonmaestros@gmail.com>
2014-02-12 22:51:50 -0600

    tempo volume things

--- Gestures/Gestures.cs
+++ Gestures/Gestures.cs
@@ -17,10 +17,10 @@ namespace Orchestra
         /// </summary>
         public static void Load()
         {
-            //gestures.Add(new VolumeGesture());
+            gestures.Add(new VolumeGesture());
             gestures.Add(new TempoGesture());
             gestures.Add(new StopGesture());
-            gestures.Add(new TempoVolume());
+            //gestures.Add(new TempoVolume());
         }
     }
 }
